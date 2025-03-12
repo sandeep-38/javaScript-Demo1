3716b5bb5709ffb890f2903e7ccdee3b0dc83968
sandeep-38 <sandeepkumarjena91@gmail.com>
2025-03-12 20:07:50 +0000

    convertion and operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,10 +27,67 @@ console.log(booleanIslogedIn); // true
 // " " => true
 //"sandeep" => true 
 
+/* 
 let someNumber=33;
 let StringNumber =String(someNumber);
 console.log(StringNumber); // 33
 
 console.log(typeof StringNumber); // string
+*/
+
+// ******************************** Operations ********************************
+
+let value=3;
+let negValue=-value;
+
+// console.log(negValue); // -3
+
+// console.log(2+2); // add // 4
+// console.log(2-2); // sub // 0
+// console.log(2*2); // mul // 4
+// console.log(2**3); // power // 8
+// console.log(2/2); // div // 0
+// console.log(2%3); // remainder //  2
+
+let str1="hello";
+let str2=" sandeep";
+let str3= str1 + str2;
+
+//  console.log(str3); // hello sandeep
+//  console.log("1" + 2); // 12
+//  console.log(1+"2"); // 12
+//  console.log("1"+2+2); // 122
+//  console.log(1+2+"2"); // 32
+//  console.log((3 + 4) * (5 % 3)); // 14
+ 
+ 
+//  console.log(+true); // 1 // bad code  
+//  console.log(+""); // 0 //  bad code 
+ 
+
+ let num1, num2,num3;
+ num1=num2=num3=2+2; // bad code 
+
+let gamecounter =100;
+++gamecounter;// 101
+console.log(gamecounter);
+gamecounter++;// 102
+console.log(gamecounter);
+
+// link to study
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+ 
+ 
+
+ 
+ 
+
+
+
+
+
+
 
 
