08aaeed986271445fafb0b61d8f9aabd34ad93dd
sandeep-38 <sandeepkumarjena91@gmail.com>
2025-03-12 19:41:55 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+let score=null ;
+
+//console.log(typeof score); // cheack the datatypes
+
+// console.log(typeof(score)); // cheack the datatypes
+
+/*
+let valueInNumber=Number(score); 
+console.log(typeof valueInNumber);  // number
+console.log(valueInNumber);  // 0
+*/
+
+// NAN = not a number 
+
+// "33" => 33 type number 
+// "33abc" =>NAN type number 
+// true =>1; false => 0
+/*
+let isLoggedIn="sandeep";
+
+let booleanIslogedIn=Boolean(isLoggedIn); 
+console.log(booleanIslogedIn); // true
+*/
+// 1 => true
+//2 => flalse 
+// "" => false
+// " " => true
+//"sandeep" => true 
+
+let someNumber=33;
+let StringNumber =String(someNumber);
+console.log(StringNumber); // 33
+
+console.log(typeof StringNumber); // string
+
+
