029e8eeeb06611d00747f172e1dc631b9535f639
sandeep-38 <sandeepkumarjena91@gmail.com>
2025-03-12 20:24:05 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,36 @@
+
+// console.log(2>1); //true
+// console.log(2>=1); // true
+// console.log(2<1); // false
+// console.log(2<=1); // false
+// console.log(2==1); // false
+// console.log(2!=1); // true
+
+// console.log("2" >1); // true // not praditable 
+// console.log("02" >1 ); // true  // not praditable 
+
+// try to compair same data type 
+
+// console.log(null > 0); // false // not praditable 
+// console.log(null == 0);// false  // not praditable 
+// console.log(null >= 0);//  true // not praditable 
+
+// console.log( undefined >0 ); // false //  not praditable
+// console.log( undefined == 0 );// false // not praditable
+// console.log( undefined >=0 ); // false // not praditable
+
+
+// '==='   is used to cheack the value as well as data types
+console.log("2"=== 2); // false 
+
+
+
+
+
+
+
+
+
+
+
+
