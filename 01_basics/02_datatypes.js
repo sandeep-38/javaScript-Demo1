3642e87ff2316099e8ed8bb91c152eee4682b0e3
sandeep-38 <sandeepkumarjena91@gmail.com>
2025-03-12 19:41:19 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,38 @@
+"use strict" // treat all js code as newer version 
+
+// alert(3+3); // we are using nodejs,not browser
+
+console.log(3
+        +3);  // code readability should be high 
+
+console.log("sandeep");
+
+
+
+let name="sandeep"; // string
+let age=20; // number
+let isLoggedIn=false; // boolean
+
+
+// number => 2 to power 53
+// bigInt => store big number 
+// string => " " 
+// boolean => true/ false 
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+
+// object => 
+// 
+
+console.log(typeof "sandeep"); // string 
+console.log(typeof age); // number 
+console.log(typeof null); // object 
+console.log(typeof undefined);// undefined 
+
+
+
+
+
+
